b74adb35a7a2c5d2b9b32fd59e4e9eb17faade18
Viktor <viktor.bubanja@hotmail.com>
2021-10-21 18:08:42 +1300

    Use built-in color functionality

--- change_git_branch/change_git_branch.go
+++ change_git_branch/change_git_branch.go
@@ -7,39 +7,38 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
-
-	"github.com/fatih/color"
 )
 
-const copy_character = "c"
+const copyCharacter = "c"
+const colorReset = "\033[0m"
+const colorGreen = "\033[32m"
 
 func main() {
-	c := color.New(color.FgGreen)
-	num_branches := 10
-	command := fmt.Sprintf("git for-each-ref --sort='-authordate:iso8601' refs/heads | head -n%d", num_branches)
+	numBranches := 10
+	command := fmt.Sprintf("git for-each-ref --sort='-authordate:iso8601' refs/heads | head -n%d", numBranches)
 	stdout, err := exec.Command("bash", "-c", command).Output()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	branches := strings.Split(string(stdout), "\n")
-	num_branches = cap(branches) - 1
-	if num_branches <= 0 {
+	numBranches = cap(branches) - 1
+	if numBranches <= 0 {
 		return
 	}
 	formatted_branches := [10]string{}
-	for i, branch := range branches[:num_branches] {
+	for i, branch := range branches[:numBranches] {
 		branch_name := strings.Fields(branch)[2]
 		branch_prefix := "refs/heads/"
 		formatted_branch := strings.TrimPrefix(branch_name, branch_prefix)
 		formatted_branches[i] = formatted_branch
-		c.Print("[" + fmt.Sprint(i) + "] ")
-		fmt.Println(formatted_branch)
+		fmt.Print(string(colorGreen), "["+fmt.Sprint(i)+"] ")
+		fmt.Println(string(colorReset), formatted_branch)
 	}
 
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Change branch?")
 	input, _ := reader.ReadString('\n')
-	if string(input[0]) == copy_character {
+	if string(input[0]) == copyCharacter {
 		number, _ := strconv.ParseInt(string(input[1]), 10, 64)
 		branch_to_copy := formatted_branches[number]
 		copy_command := fmt.Sprintf("echo %s | pbcopy", branch_to_copy)
